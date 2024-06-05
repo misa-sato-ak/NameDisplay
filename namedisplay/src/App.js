6d5db42331cdc98ed9f37cd88e500d566ddead35
misa-sato-ak <akmisa519@gmail.com>
2024-06-05 13:12:31 +0900

    066

--- namedisplay/src/App.js
+++ namedisplay/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NameDisplay from './NameDisplay';
 
-function App() {
+const App = () => {
+  const name = 'React Practice';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="app-heading">名前表示アプリ</h1>
+      <NameDisplay name={name} />
     </div>
   );
-}
+};
 
-export default App;
+export default App;  
